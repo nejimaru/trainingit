7b2207f24de27edb84e6236a1099059456483012
nejimaru <ibni.yamine.chabane@gmail.com>
2016-12-02 14:12:57 +0100

    ajout de script js

---
+++ index.js
@@ -0,0 +1,25 @@
+var fn_init = function () {}
+let lignes Cdes = [];
+let clients = [{name: 'client 1', code : "cl1"}
+                      { name : 'client2', code : 'cl2'} ];
+let TVA = [2, 10, 20.6];for (let i = 0 ; i < 1000 ; i ++) }
+    let idxTva = Math.round(
+                 Math.random()*2);
+    let myTva  = TVA[idxTva];
+    let idxCli =Math.round(Math.random()*5);
+    let myCli  = clients [idxCli];
+    let montHT = Math.random()*1000;lignesCdes.push(
+ [myCli, myTVA, montHT]);}
+console.log (lignesCdes);
+return lignesCdes;
+}				"use strict"
+
+let lignesCodes = [];
+let clients = [{name: "Client1", code:"Cl1"},
+	{name: "Client2", code: "Cl2"}];
+
+let TVA = [2,10,20.6];
+
+for (let i=0;i<1000;i++) {
+	let   = Math.round(Math.random()*2);
+	let mytva = TVA[id]
